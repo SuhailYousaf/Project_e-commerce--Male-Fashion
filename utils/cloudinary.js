1d6cb6e4473881fd719a0c6a0cdaef1c1a4cf583
suhail_yousaf <ysuhail444@gmail.com>
2023-05-04 20:02:47 +0530

    test initial

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -1,9 +1,9 @@
 const cloudinary = require('cloudinary').v2;
 
 cloudinary.config({
-    cloud_name: 'dklwpvial',
-    api_key: 239179456795758,
-    api_secret: 'n2xIVJ53ukMruOkHmjOKNlln4i8'
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY ,
+    api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
 module.exports = cloudinary;
